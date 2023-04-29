94b7a24c94586a6ee83edb3b90a54baffd7e86b4
Denys Victorovich Matveev <strigon33@gmail.com>
2023-04-29 14:56:18 +0900

    Fix type error in pixi root

--- src/PixiRoot.tsx
+++ src/PixiRoot.tsx
@@ -1,10 +1,9 @@
-import { Application, Texture, Sprite } from "pixi.js";
 import { useRef, useEffect } from "react";
 import * as React from "react";
 import * as PIXI from 'pixi.js'
 
-export const PixiRoot = (props: {app: PIXI.Application}) => {
-	const ref: React.useRef<HTMLDivElement> = useRef(null);
+export const PixiRoot = (props: { app: PIXI.Application }) => {
+	const ref = useRef<HTMLDivElement>(null);
 	useEffect(() => {
 		let view = props.app.view;
 		ref.current.appendChild(view);
