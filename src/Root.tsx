45358ea8c1fc9b3a6e244973e1b8e82abe809e4f
martoko <18300234+martoko@users.noreply.github.com>
2023-04-29 19:35:55 +0900

    Don't limit fps

--- src/Root.tsx
+++ src/Root.tsx
@@ -216,6 +216,8 @@ const roomHandlesDrawQueue = roomHandles.flat().reverse()
 let gloopAmount = 100;
 let landingGearFuel = 0;
 const requiredLandingGearFuel = 50;
+let elapsedTimeBetweenGloopMovement = 0;
+let gloopMovementInterval = 25;
 
 export function Root() {
 	const [gloopAmountValue, setGloopAmount] = useState(100);
@@ -235,156 +237,305 @@ export function Root() {
 				}
 			}
 
-			const previous = roomHandles.map(row => row.map(row => row.data))
-			for (let x = 0; x < 6; x++) {
-				for (let y = 0; y < 4; y++) {
-					roomHandles[y][x].data = { ...previous[y][x] };
+			elapsedTimeBetweenGloopMovement += delta;
 
-					const candidatePressure = candidate =>
-						roomHandles[candidate.y][candidate.x].data[candidate.pipe] / roomHandles[candidate.y][candidate.x].data[candidate.pipeCapacity];
+			if (elapsedTimeBetweenGloopMovement > gloopMovementInterval) {
+				elapsedTimeBetweenGloopMovement = 0;
+				const previous = roomHandles.map(row => row.map(row => row.data))
+				for (let x = 0; x < 6; x++) {
+					for (let y = 0; y < 4; y++) {
+						roomHandles[y][x].data = {...previous[y][x]};
 
-					for (let [pipe, pipeCapacity] of [['rightPipe', 'rightPipeCapacity'], ['bottomPipe', 'bottomPipeCapacity']]) {
-						console.assert(previous[y][x][pipe] <= previous[y][x][pipeCapacity]);
-						if (previous[y][x][pipe] > 0) {
-							let candidates =
-								pipe == 'rightPipe' ? [
-									// Left-side bottom
-									{ x: x, y: y, pipe: 'bottomPipe', pipeCapacity: 'bottomPipeCapacity', isOpen: previous[y][x].bottomOpen },
-									// Left-side left
-									{ x: x - 1, y: y, pipe: 'rightPipe', pipeCapacity: 'rightPipeCapacity', isOpen: previous[y][x].leftOpen },
-									// Left-side up
-									{ x: x, y: y - 1, pipe: 'bottomPipe', pipeCapacity: 'bottomPipeCapacity', isOpen: previous[y][x].topOpen },
+						const candidatePressure = candidate =>
+							roomHandles[candidate.y][candidate.x].data[candidate.pipe] / roomHandles[candidate.y][candidate.x].data[candidate.pipeCapacity];
 
-									// Right-side bottom
-									{ x: x + 1, y: y, pipe: 'bottomPipe', pipeCapacity: 'bottomPipeCapacity', isOpen: previous[y][x + 1].bottomOpen },
-									// Right-side right
-									{ x: x + 1, y: y, pipe: 'rightPipe', pipeCapacity: 'rightPipeCapacity', isOpen: previous[y][x + 1].rightOpen },
-									// Right-side up
-									{ x: x + 1, y: y - 1, pipe: 'bottomPipe', pipeCapacity: 'bottomPipeCapacity', isOpen: previous[y][x + 1].topOpen },
-								] : [
-									// Top-side left
-									{ x: x - 1, y: y, pipe: 'rightPipe', pipeCapacity: 'rightPipeCapacity', isOpen: previous[y][x].leftOpen },
-									// Top-side up
-									{ x: x, y: y - 1, pipe: 'bottomPipe', pipeCapacity: 'bottomPipeCapacity', isOpen: previous[y][x].topOpen },
-									// Top-side right
-									{ x: x, y: y, pipe: 'rightPipe', pipeCapacity: 'rightPipeCapacity', isOpen: previous[y][x].rightOpen },
+						for (let [pipe, pipeCapacity] of [['rightPipe', 'rightPipeCapacity'], ['bottomPipe', 'bottomPipeCapacity']]) {
+							console.assert(previous[y][x][pipe] <= previous[y][x][pipeCapacity]);
+							if (previous[y][x][pipe] > 0) {
+								let candidates =
+									pipe == 'rightPipe' ? [
+										// Left-side bottom
+										{
+											x: x,
+											y: y,
+											pipe: 'bottomPipe',
+											pipeCapacity: 'bottomPipeCapacity',
+											isOpen: previous[y][x].bottomOpen
+										},
+										// Left-side left
+										{
+											x: x - 1,
+											y: y,
+											pipe: 'rightPipe',
+											pipeCapacity: 'rightPipeCapacity',
+											isOpen: previous[y][x].leftOpen
+										},
+										// Left-side up
+										{
+											x: x,
+											y: y - 1,
+											pipe: 'bottomPipe',
+											pipeCapacity: 'bottomPipeCapacity',
+											isOpen: previous[y][x].topOpen
+										},
 
-									// Bottom-side bottom
-									{ x: x, y: y + 1, pipe: 'bottomPipe', pipeCapacity: 'bottomPipeCapacity', isOpen: previous[y + 1][x].bottomOpen },
-									// Bottom-side left
-									{ x: x - 1, y: y + 1, pipe: 'rightPipe', pipeCapacity: 'rightPipeCapacity', isOpen: previous[y + 1][x].leftOpen },
-									// Bottom-side right
-									{ x: x, y: y + 1, pipe: 'rightPipe', pipeCapacity: 'rightPipeCapacity', isOpen: previous[y + 1][x].rightOpen },
-								];
+										// Right-side bottom
+										{
+											x: x + 1,
+											y: y,
+											pipe: 'bottomPipe',
+											pipeCapacity: 'bottomPipeCapacity',
+											isOpen: previous[y][x + 1].bottomOpen
+										},
+										// Right-side right
+										{
+											x: x + 1,
+											y: y,
+											pipe: 'rightPipe',
+											pipeCapacity: 'rightPipeCapacity',
+											isOpen: previous[y][x + 1].rightOpen
+										},
+										// Right-side up
+										{
+											x: x + 1,
+											y: y - 1,
+											pipe: 'bottomPipe',
+											pipeCapacity: 'bottomPipeCapacity',
+											isOpen: previous[y][x + 1].topOpen
+										},
+									] : [
+										// Top-side left
+										{
+											x: x - 1,
+											y: y,
+											pipe: 'rightPipe',
+											pipeCapacity: 'rightPipeCapacity',
+											isOpen: previous[y][x].leftOpen
+										},
+										// Top-side up
+										{
+											x: x,
+											y: y - 1,
+											pipe: 'bottomPipe',
+											pipeCapacity: 'bottomPipeCapacity',
+											isOpen: previous[y][x].topOpen
+										},
+										// Top-side right
+										{
+											x: x,
+											y: y,
+											pipe: 'rightPipe',
+											pipeCapacity: 'rightPipeCapacity',
+											isOpen: previous[y][x].rightOpen
+										},
 
-							// Let's limit our water movement to max 1 per room for now
-							candidates = candidates.filter(candidate =>
-								candidate.x >= 0 && candidate.x < 6 && candidate.y >= 0 && candidate.y < 4
-								&& roomHandles[candidate.y][candidate.x].data[candidate.pipe] < roomHandles[candidate.y][candidate.x].data[candidate.pipeCapacity]
-								&& candidatePressure(candidate) < (roomHandles[y][x].data[pipe] / roomHandles[y][x].data[pipeCapacity])
-								&& candidate.isOpen
-							);
-							if (candidates.length > 0) {
-								candidates = candidates.sort(
-									(a, b) => candidatePressure(a) - candidatePressure(b)
+										// Bottom-side bottom
+										{
+											x: x,
+											y: y + 1,
+											pipe: 'bottomPipe',
+											pipeCapacity: 'bottomPipeCapacity',
+											isOpen: previous[y + 1][x].bottomOpen
+										},
+										// Bottom-side left
+										{
+											x: x - 1,
+											y: y + 1,
+											pipe: 'rightPipe',
+											pipeCapacity: 'rightPipeCapacity',
+											isOpen: previous[y + 1][x].leftOpen
+										},
+										// Bottom-side right
+										{
+											x: x,
+											y: y + 1,
+											pipe: 'rightPipe',
+											pipeCapacity: 'rightPipeCapacity',
+											isOpen: previous[y + 1][x].rightOpen
+										},
+									];
+
+								// Let's limit our water movement to max 1 per room for now
+								candidates = candidates.filter(candidate =>
+									candidate.x >= 0 && candidate.x < 6 && candidate.y >= 0 && candidate.y < 4
+									&& roomHandles[candidate.y][candidate.x].data[candidate.pipe] < roomHandles[candidate.y][candidate.x].data[candidate.pipeCapacity]
+									&& candidatePressure(candidate) < (roomHandles[y][x].data[pipe] / roomHandles[y][x].data[pipeCapacity])
+									&& candidate.isOpen
 								);
-								candidates = candidates.filter(
-									candidate => candidatePressure(candidate) == candidatePressure(candidates[0])
-								);
-								const candidate = candidates[Math.floor(Math.random() * candidates.length)];
-								roomHandles[candidate.y][candidate.x].data[candidate.pipe] += 1;
-								roomHandles[y][x].data[pipe] -= 1;
+								if (candidates.length > 0) {
+									candidates = candidates.sort(
+										(a, b) => candidatePressure(a) - candidatePressure(b)
+									);
+									candidates = candidates.filter(
+										candidate => candidatePressure(candidate) == candidatePressure(candidates[0])
+									);
+									const candidate = candidates[Math.floor(Math.random() * candidates.length)];
+									roomHandles[candidate.y][candidate.x].data[candidate.pipe] += 1;
+									roomHandles[y][x].data[pipe] -= 1;
+								}
 							}
 						}
-					}
 
-					if (previous[y][x].feature === 'source') {
-						let waterLeft = gloopAmount - 2 >= 0 ? 2 : 0;
-						gloopAmount -= waterLeft;
-						setGloopAmount(gloopAmount);
-						let candidates = [
-							// Bottom
-							{ x: x, y: y, pipe: 'bottomPipe', pipeCapacity: 'bottomPipeCapacity', isOpen: previous[y][x].bottomOpen },
-							// Left
-							{ x: x - 1, y: y, pipe: 'rightPipe', pipeCapacity: 'rightPipeCapacity', isOpen: previous[y][x].leftOpen },
-							// Top
-							{ x: x, y: y - 1, pipe: 'bottomPipe', pipeCapacity: 'bottomPipeCapacity', isOpen: previous[y][x].topOpen },
-							// Right
-							{ x: x, y: y, pipe: 'rightPipe', pipeCapacity: 'rightPipeCapacity', isOpen: previous[y][x].rightOpen },
-						];
-						while (candidates.length > 0 && waterLeft > 0) {
-							candidates = candidates.filter(candidate =>
-								candidate.x >= 0 && candidate.x < 6 && candidate.y >= 0 && candidate.y < 4
-								&& roomHandles[candidate.y][candidate.x].data[candidate.pipe] < roomHandles[candidate.y][candidate.x].data[candidate.pipeCapacity]
-								&& candidate.isOpen
-							);
-							if (candidates.length > 0) {
-								candidates = candidates.sort(
-									(a, b) => candidatePressure(a) - candidatePressure(b)
+						if (previous[y][x].feature === 'source') {
+							let waterLeft = gloopAmount - 2 >= 0 ? 2 : 0;
+							gloopAmount -= waterLeft;
+							setGloopAmount(gloopAmount);
+							let candidates = [
+								// Bottom
+								{
+									x: x,
+									y: y,
+									pipe: 'bottomPipe',
+									pipeCapacity: 'bottomPipeCapacity',
+									isOpen: previous[y][x].bottomOpen
+								},
+								// Left
+								{
+									x: x - 1,
+									y: y,
+									pipe: 'rightPipe',
+									pipeCapacity: 'rightPipeCapacity',
+									isOpen: previous[y][x].leftOpen
+								},
+								// Top
+								{
+									x: x,
+									y: y - 1,
+									pipe: 'bottomPipe',
+									pipeCapacity: 'bottomPipeCapacity',
+									isOpen: previous[y][x].topOpen
+								},
+								// Right
+								{
+									x: x,
+									y: y,
+									pipe: 'rightPipe',
+									pipeCapacity: 'rightPipeCapacity',
+									isOpen: previous[y][x].rightOpen
+								},
+							];
+							while (candidates.length > 0 && waterLeft > 0) {
+								candidates = candidates.filter(candidate =>
+									candidate.x >= 0 && candidate.x < 6 && candidate.y >= 0 && candidate.y < 4
+									&& roomHandles[candidate.y][candidate.x].data[candidate.pipe] < roomHandles[candidate.y][candidate.x].data[candidate.pipeCapacity]
+									&& candidate.isOpen
 								);
-								roomHandles[candidates[0].y][candidates[0].x].data[candidates[0].pipe] += 1;
-								waterLeft -= 1;
+								if (candidates.length > 0) {
+									candidates = candidates.sort(
+										(a, b) => candidatePressure(a) - candidatePressure(b)
+									);
+									roomHandles[candidates[0].y][candidates[0].x].data[candidates[0].pipe] += 1;
+									waterLeft -= 1;
+								}
 							}
 						}
-					}
 
-					if (previous[y][x].feature === 'sink') {
-						let waterToConsume = 1;
-						let candidates = [
-							// Bottom
-							{ x: x, y: y, pipe: 'bottomPipe', pipeCapacity: 'bottomPipeCapacity', isOpen: previous[y][x].bottomOpen },
-							// Left
-							{ x: x - 1, y: y, pipe: 'rightPipe', pipeCapacity: 'rightPipeCapacity', isOpen: previous[y][x].leftOpen },
-							// Top
-							{ x: x, y: y - 1, pipe: 'bottomPipe', pipeCapacity: 'bottomPipeCapacity', isOpen: previous[y][x].topOpen },
-							// Right
-							{ x: x, y: y, pipe: 'rightPipe', pipeCapacity: 'rightPipeCapacity', isOpen: previous[y][x].rightOpen },
-						];
-						while (candidates.length > 0 && waterToConsume > 0) {
-							candidates = candidates.filter(candidate =>
-								candidate.x >= 0 && candidate.x < 6 && candidate.y >= 0 && candidate.y < 4
-								&& roomHandles[candidate.y][candidate.x].data[candidate.pipe] > 0
-								&& candidate.isOpen
-							);
-							if (candidates.length > 0) {
-								candidates = candidates.sort(
-									(a, b) => candidatePressure(a) - candidatePressure(b)
+						if (previous[y][x].feature === 'sink') {
+							let waterToConsume = 1;
+							let candidates = [
+								// Bottom
+								{
+									x: x,
+									y: y,
+									pipe: 'bottomPipe',
+									pipeCapacity: 'bottomPipeCapacity',
+									isOpen: previous[y][x].bottomOpen
+								},
+								// Left
+								{
+									x: x - 1,
+									y: y,
+									pipe: 'rightPipe',
+									pipeCapacity: 'rightPipeCapacity',
+									isOpen: previous[y][x].leftOpen
+								},
+								// Top
+								{
+									x: x,
+									y: y - 1,
+									pipe: 'bottomPipe',
+									pipeCapacity: 'bottomPipeCapacity',
+									isOpen: previous[y][x].topOpen
+								},
+								// Right
+								{
+									x: x,
+									y: y,
+									pipe: 'rightPipe',
+									pipeCapacity: 'rightPipeCapacity',
+									isOpen: previous[y][x].rightOpen
+								},
+							];
+							while (candidates.length > 0 && waterToConsume > 0) {
+								candidates = candidates.filter(candidate =>
+									candidate.x >= 0 && candidate.x < 6 && candidate.y >= 0 && candidate.y < 4
+									&& roomHandles[candidate.y][candidate.x].data[candidate.pipe] > 0
+									&& candidate.isOpen
 								);
-								roomHandles[candidates[0].y][candidates[0].x].data[candidates[0].pipe] -= 1;
-								waterToConsume -= 1;
+								if (candidates.length > 0) {
+									candidates = candidates.sort(
+										(a, b) => candidatePressure(a) - candidatePressure(b)
+									);
+									roomHandles[candidates[0].y][candidates[0].x].data[candidates[0].pipe] -= 1;
+									waterToConsume -= 1;
+								}
 							}
 						}
-					}
 
-					if (previous[y][x].feature === 'landingGear' && landingGearFuel < requiredLandingGearFuel) {
-						let gloopToConsume = 1;
-						let candidates = [
-							// Bottom
-							{ x: x, y: y, pipe: 'bottomPipe', pipeCapacity: 'bottomPipeCapacity', isOpen: previous[y][x].bottomOpen },
-							// Left
-							{ x: x - 1, y: y, pipe: 'rightPipe', pipeCapacity: 'rightPipeCapacity', isOpen: previous[y][x].leftOpen },
-							// Top
-							{ x: x, y: y - 1, pipe: 'bottomPipe', pipeCapacity: 'bottomPipeCapacity', isOpen: previous[y][x].topOpen },
-							// Right
-							{ x: x, y: y, pipe: 'rightPipe', pipeCapacity: 'rightPipeCapacity', isOpen: previous[y][x].rightOpen },
-						];
-						while (candidates.length > 0 && gloopToConsume > 0) {
-							candidates = candidates.filter(candidate =>
-								candidate.x >= 0 && candidate.x < 6 && candidate.y >= 0 && candidate.y < 4
-								&& roomHandles[candidate.y][candidate.x].data[candidate.pipe] > 0
-								&& candidate.isOpen
-							);
-							if (candidates.length > 0) {
-								candidates = candidates.sort(
-									(a, b) => candidatePressure(a) - candidatePressure(b)
+						if (previous[y][x].feature === 'landingGear' && landingGearFuel < requiredLandingGearFuel) {
+							let gloopToConsume = 1;
+							let candidates = [
+								// Bottom
+								{
+									x: x,
+									y: y,
+									pipe: 'bottomPipe',
+									pipeCapacity: 'bottomPipeCapacity',
+									isOpen: previous[y][x].bottomOpen
+								},
+								// Left
+								{
+									x: x - 1,
+									y: y,
+									pipe: 'rightPipe',
+									pipeCapacity: 'rightPipeCapacity',
+									isOpen: previous[y][x].leftOpen
+								},
+								// Top
+								{
+									x: x,
+									y: y - 1,
+									pipe: 'bottomPipe',
+									pipeCapacity: 'bottomPipeCapacity',
+									isOpen: previous[y][x].topOpen
+								},
+								// Right
+								{
+									x: x,
+									y: y,
+									pipe: 'rightPipe',
+									pipeCapacity: 'rightPipeCapacity',
+									isOpen: previous[y][x].rightOpen
+								},
+							];
+							while (candidates.length > 0 && gloopToConsume > 0) {
+								candidates = candidates.filter(candidate =>
+									candidate.x >= 0 && candidate.x < 6 && candidate.y >= 0 && candidate.y < 4
+									&& roomHandles[candidate.y][candidate.x].data[candidate.pipe] > 0
+									&& candidate.isOpen
 								);
-								roomHandles[candidates[0].y][candidates[0].x].data[candidates[0].pipe] -= 1;
-								gloopToConsume -= 1;
-								landingGearFuel += 1;
+								if (candidates.length > 0) {
+									candidates = candidates.sort(
+										(a, b) => candidatePressure(a) - candidatePressure(b)
+									);
+									roomHandles[candidates[0].y][candidates[0].x].data[candidates[0].pipe] -= 1;
+									gloopToConsume -= 1;
+									landingGearFuel += 1;
+								}
 							}
 						}
+						setLandingGearFuel(landingGearFuel);
 					}
-					setLandingGearFuel(landingGearFuel);
 				}
 			}
 
@@ -392,7 +543,6 @@ export function Root() {
 				DrawRoom(room)
 		};
 
-		app.ticker.maxFPS = 0.1;
 		app.ticker.add(gameLoop);
 		window.addEventListener('keydown', keyDownListener, false);
 		return () => {
