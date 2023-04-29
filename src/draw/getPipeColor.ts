2c36f2ca0d6416aa43866ff6fdf78d91337da6a5
martoko <18300234+martoko@users.noreply.github.com>
2023-04-29 22:05:49 +0900

    Increase contrast between empty pipes and fluid filled pipes

--- src/draw/getPipeColor.ts
+++ src/draw/getPipeColor.ts
@@ -3,5 +3,9 @@ import { lerpColor } from "./lerpColor";
 
 
 export function getPipeColor(fluid: number, capacity: number): PIXI.Color {
-	return lerpColor(new PIXI.Color(0x000000), new PIXI.Color(0x0000ff), fluid / capacity);
+	if (fluid == 0) {
+		return new PIXI.Color(0x000000);
+	} else {
+		return lerpColor(new PIXI.Color(0x7777ff), new PIXI.Color(0x1111aa), fluid / capacity);
+	}
 }
