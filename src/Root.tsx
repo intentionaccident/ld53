83133bc831b3d3569f1279010a3711e01fbdd13c
martoko <18300234+martoko@users.noreply.github.com>
2023-04-29 16:58:18 +0900

    Use water pressure to propagate water

--- src/Root.tsx
+++ src/Root.tsx
@@ -49,6 +49,8 @@ interface Room {
 
 function DrawRoom(room: RoomHandle) {
 	const graphics = room.graphics.pipes;
+
+	graphics.clear();
 	// Draw intersection dot
 	graphics.beginFill(room.data.isSource ? 0x009999 : 0x999999);
 	graphics.lineStyle(4, 0x00FFFF, 1);
@@ -119,9 +121,9 @@ const roomHandles: RoomHandle[][] = Array.from({ length: 4 }, (_, y) =>
 			coordinate: new PIXI.Point(x, y),
 			data: {
 				bottomPipe: 0,
-				bottomPipeCapacity: y == 3 ? 0 : 2,
+				bottomPipeCapacity: y == 3 ? 0 : 5,
 				rightPipe: 0,
-				rightPipeCapacity: x == 5 ? 0 : 2,
+				rightPipeCapacity: x == 5 ? 0 : 5,
 
 				topOpen: true,
 				bottomOpen: true,
@@ -165,11 +167,14 @@ export function Root() {
 				for (let y = 0; y < 4; y++) {
 					roomHandles[y][x].data = { ...previous[y][x] };
 
+					const candidatePressure = candidate =>
+						roomHandles[candidate.y][candidate.x].data[candidate.pipe] / roomHandles[candidate.y][candidate.x].data[candidate.pipeCapacity];
+
 					for (let [pipe, pipeCapacity] of [['rightPipe', 'rightPipeCapacity'], ['bottomPipe', 'bottomPipeCapacity']]) {
 						console.assert(previous[y][x][pipe] <= previous[y][x][pipeCapacity]);
-						if (previous[y][x][pipe] === previous[y][x][pipeCapacity]) {
+						if (previous[y][x][pipe] > 1) {
 							// TODO: Check for intersection openness
-							const candidates =
+							let candidates =
 								pipe == 'rightPipe' ? [
 									// Left-side bottom
 									{ x: x, y: y, pipe: 'bottomPipe', pipeCapacity: 'bottomPipeCapacity' },
@@ -200,21 +205,20 @@ export function Root() {
 									{ x: x, y: y + 1, pipe: 'rightPipe', pipeCapacity: 'rightPipeCapacity' },
 								];
 
-							// TODO: Should run until out of good candidates (candidates where the pressure dictates we move water towards them)
-							for (const candidate of candidates) {
-								// Break when out of water
-								if (roomHandles[y][x].data[pipe] < roomHandles[y][x].data[pipeCapacity]) break;
-								// Ignore out of bounds
-								if (candidate.x < 0 || candidate.x >= 6 || candidate.y < 0 || candidate.y >= 4) {
-									continue;
-								}
-								if (
-									// TODO: Should be comparing pressure here instead
-									previous[candidate.y][candidate.x][candidate.pipe] >= previous[candidate.y][candidate.x][candidate.pipeCapacity]
-									|| roomHandles[candidate.y][candidate.x].data[candidate.pipe] >= roomHandles[candidate.y][candidate.x].data[candidate.pipeCapacity]
-								) {
-									continue;
-								}
+							// Let's limit our water movement to max 1 per room for now
+							candidates = candidates.filter(candidate =>
+								candidate.x >= 0 && candidate.x < 6 && candidate.y >= 0 && candidate.y < 4
+								&& roomHandles[candidate.y][candidate.x].data[candidate.pipe] < roomHandles[candidate.y][candidate.x].data[candidate.pipeCapacity]
+								&& candidatePressure(candidate) < (roomHandles[y][x].data[pipe] / roomHandles[y][x].data[pipeCapacity])
+							);
+							if (candidates.length > 0) {
+								candidates = candidates.sort(
+									(a, b) => candidatePressure(a) - candidatePressure(b)
+								);
+								candidates = candidates.filter(
+									candidate => candidatePressure(candidate) == candidatePressure(candidates[0])
+								);
+								const candidate = candidates[Math.floor(Math.random() * candidates.length)];
 								roomHandles[candidate.y][candidate.x].data[candidate.pipe] += 1;
 								roomHandles[y][x].data[pipe] -= 1;
 							}
@@ -222,9 +226,9 @@ export function Root() {
 					}
 
 					if (previous[y][x].isSource) {
-						let waterLeft = 1;
+						let waterLeft = 2;
 						// TODO: Check for intersection openness
-						const candidates = [
+						let candidates = [
 							// Bottom
 							{ x: x, y: y, pipe: 'bottomPipe', pipeCapacity: 'bottomPipeCapacity' },
 							// Left
@@ -233,14 +237,28 @@ export function Root() {
 							{ x: x, y: y - 1, pipe: 'bottomPipe', pipeCapacity: 'bottomPipeCapacity' },
 							// Right
 							{ x: x, y: y, pipe: 'rightPipe', pipeCapacity: 'rightPipeCapacity' },
-						].filter(candidate =>
-							candidate.x >= 0 && candidate.x < 6 && candidate.y >= 0 && candidate.y < 4
-							&& roomHandles[candidate.y][candidate.x].data[candidate.pipe] < roomHandles[candidate.y][candidate.x].data[candidate.pipeCapacity]
-						);
-						for (const candidate of candidates) {
-							if (waterLeft <= 0) break;
-							roomHandles[candidate.y][candidate.x].data[candidate.pipe] += 1;
-							waterLeft -= 1;
+						];
+						// .filter(candidate =>
+						// 	candidate.x >= 0 && candidate.x < 6 && candidate.y >= 0 && candidate.y < 4
+						// 	&& roomHandles[candidate.y][candidate.x].data[candidate.pipe] < roomHandles[candidate.y][candidate.x].data[candidate.pipeCapacity]
+						// );
+						// for (const candidate of candidates) {
+						// 	if (waterLeft <= 0) break;
+						// 	roomHandles[candidate.y][candidate.x].data[candidate.pipe] += 1;
+						// 	waterLeft -= 1;
+						// }
+						while (candidates.length > 0 && waterLeft > 0) {
+							candidates = candidates.filter(candidate =>
+								candidate.x >= 0 && candidate.x < 6 && candidate.y >= 0 && candidate.y < 4
+								&& roomHandles[candidate.y][candidate.x].data[candidate.pipe] < roomHandles[candidate.y][candidate.x].data[candidate.pipeCapacity]
+							);
+							if (candidates.length > 0) {
+								candidates = candidates.sort(
+									(a, b) => candidatePressure(a) - candidatePressure(b)
+								);
+								roomHandles[candidates[0].y][candidates[0].x].data[candidates[0].pipe] += 1;
+								waterLeft -= 1;
+							}
 						}
 					}
 				}
